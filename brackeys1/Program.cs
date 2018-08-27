eb493ded2916a6980e422d8d1fdd40acdf804832
reubenjh <rjharcourt@gmail.com>
2018-08-27 16:42:57 +1200

    basic calc

--- brackeys1/Program.cs
+++ brackeys1/Program.cs
@@ -10,6 +10,17 @@ namespace brackeys1
     {
         static void Main(string[] args)
         {
+            int num01;
+            int num02;
+
+            Console.Write("Type a number to be multiplied: ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The result is " + num01 * num02);
+            Console.ReadKey();
         }
     }
 }
