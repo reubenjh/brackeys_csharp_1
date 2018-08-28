9c80a757b3f0fd0236b88ce09664be7ba3a0f9d5
reubenjh <rjharcourt@gmail.com>
2018-08-28 13:41:23 +1200

    calculator multiply, using goto, switching

--- brackeys1/Program.cs
+++ brackeys1/Program.cs
@@ -12,15 +12,45 @@ namespace brackeys1
         {
             int num01;
             int num02;
+            string operation;
 
+            Start:
+            // Ask what calc
+            Console.WriteLine("What calc would you like to do?");
+            Console.WriteLine("Press + for add");
+            Console.WriteLine("Press - for subtract");
+            Console.WriteLine("Press * for multiply");
+            Console.WriteLine("Press / for divide");
+            operation = Convert.ToString(Console.ReadLine());
+            switch (operation)
+            {
+                case "*":
+                    multiply(out num01, out num02);
+                    break;
+                default:
+                    Console.WriteLine("You pressed a dumb key...");
+                    Console.WriteLine();
+                    break;
+            }
+
+            goto Start;
+        }
+
+        private static void multiply(out int num01, out int num02)
+        {
             Console.Write("Type a number to be multiplied: ");
             num01 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Type another number: ");
+            Console.Write("Type another number to multiply by: ");
             num02 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("The result is " + num01 * num02);
+            Console.WriteLine("press <any key> to go again");
+
+            // wait for user to press key to continue
             Console.ReadKey();
+            Console.WriteLine();
+            
         }
     }
 }
